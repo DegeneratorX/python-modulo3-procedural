e9ca8a6244b42dc77b30fe2a5e653f3ce8ce378e
Victor <victorio.deg@gmail.com>
2025-11-18 21:11:38 -0300

    Ola

--- aula8-comprehensions/4_dict_set_comp.py
+++ aula8-comprehensions/4_dict_set_comp.py
@@ -1,11 +1,35 @@
+"""
+Dictionary e Set Comprehensions
+
+Posso usar comprehensions para criar dicionários e sets também.
+"""
+
+"""
+Criando dicionários com dictionary comprehension
+"""
+
 lista = [
     ('chave', 'valor'),
     ('chave2', 'valor2')
 ]
 
-d1 = {x: y for x, y in lista}  # Criando dicionário a partir de uma lista contendo pares de tuplas.
+# Mapeando dicionário a partir de uma lista contendo pares de tuplas.
 # Para dictionary comprehension, usa-se x: y entre chaves
+d1 = {x: y for x, y in lista}
+
+print("d1 criada:", d1)
+
+
+
+
+
+
+print("\n#############################################\n")
+"""
+Criando sets com set comprehension
+"""
 
 # Já no caso de criação de set comprehension, usa-se x apenas.
-
-s1 = {x for x, y in lista}  # Criando set, também entre chaves, mas sem x: y, e sim apenas x.
+# Criando set, também entre chaves, mas sem x: y, e sim apenas x.
+s1 = {x for x, y in lista} 
+print("s1 criada:", s1)
